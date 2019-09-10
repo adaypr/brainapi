2b2b169600468f8b0434ce6959bfc521b4446441
adaypr <38984581+adaypr@users.noreply.github.com>
2019-09-10 13:45:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,16 +24,7 @@ def get_tasks():
 	
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
 def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': task, 'request': request.json}), 201
+    return jsonify({'request': request.json}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
