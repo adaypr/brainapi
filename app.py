bd884ee682e701dd55a12bdd46c4a2538d0243d8
adaypr <38984581+adaypr@users.noreply.github.com>
2019-09-10 20:38:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def create_task():
     #print('Texto: ' + request.form['Body'])
     #print('From: ' + request.form['From'])
     #print('To: ' + request.form['To'])
-    print(request.json)
+    print(request.args)
     return jsonify({'request': request.json}), 201
 
 if __name__ == '__main__':
