f5119bbc9d0d09b3c7b62428a09f0c73861acad5
adaypr <38984581+adaypr@users.noreply.github.com>
2019-09-10 16:39:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def get_tasks():
 	
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
 def create_task():
-    print(jsonify({'request': request.json}))
+    print(request)
     return jsonify({'request': request.json}), 201
 
 if __name__ == '__main__':
