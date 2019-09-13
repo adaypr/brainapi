f7580b6ee1d2f4d97d2f460cfd5a877501cf3eb4
adaypr <38984581+adaypr@users.noreply.github.com>
2019-09-13 02:43:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from detect_intent_texts import detect_intent_texts
 import uuid
 from twilio.rest import Client
 from sendWhatsapp import send_WhatsApp
+from salesforceIntegration import salesforce_Autentication, salesforce_LiveChatTranscript
 
 app = Flask(__name__)
 
@@ -19,14 +20,18 @@ def create_task():
     #print(request.form)   
     
     #Envia Datos DialogFlow
-    fulfillment_text = detect_intent_texts("chatbotapiintegration-hvlbfm", request.form['From'].replace("whatsapp:+",""), [request.form['Body']], "en-US")
+    fulfillment_text = detect_intent_texts("chatbotapiintegration-hvlbfm", request.form['SmsMessageSid'], [request.form['Body']], "en-US")
     print('Texto: ' + fulfillment_text)
     
     #Envia Respuesta por Whatsapp
     send_WhatsApp(request.form['From'], request.form['To'], fulfillment_text)
     
+    #Envia Pregunta y Respuesta a Salesforce
+    authtoken = salesforce_Autentication()
+    result = salesforce_LiveChatTranscript(fulfillment_text, authtoken)
+    print('Resultado: ' + result)
+    
     return jsonify({'request': request.form}), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
- 
