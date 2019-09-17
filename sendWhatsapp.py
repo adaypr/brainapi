da01c8ae791c225b34ccf57090ba5b0b7efd16be
adaypr <38984581+adaypr@users.noreply.github.com>
2019-09-17 09:37:55 +0200

    Update sendWhatsapp.py

--- sendWhatsapp.py
+++ sendWhatsapp.py
@@ -5,9 +5,14 @@ Created on 10 sept. 2019
 '''
 from twilio.rest import Client
 
-def send_WhatsApp(to_whatsapp_number, from_whatsapp_number, text_to_send):
+def send_WhatsApp(to_whatsapp_number, from_whatsapp_number, text_to_send, media_url):
     # client credentials are read from TWILIO_ACCOUNT_SID and AUTH_TOKEN
-    client = Client()    
-    client.messages.create(body=text_to_send,
+    client = Client()
+    if media_url != '':
+        client.messages.create(body=text_to_send,
+                           from_=from_whatsapp_number,
+                           to=to_whatsapp_number, media_url=media_url)
+    else:
+        client.messages.create(body=text_to_send,
                            from_=from_whatsapp_number,
                            to=to_whatsapp_number)
