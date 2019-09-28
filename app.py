b42d16eaf80d7ca130361dc867f7a2a112253761
adaypr <38984581+adaypr@users.noreply.github.com>
2019-09-28 11:39:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,9 @@ def receive_message():
         return verify_fb_token(token_sent)
     #if the request was not get, it must be POST and we can just proceed with sending a message back to user
     else:
-        # get whatever message a user sent the bot
+        # get whatever message a user sent the bot    
+        data = {}
+        data['channel'] = "facebookMessenger"    
         output = request.get_json()
         print(output)
         for event in output['entry']:
@@ -32,13 +34,17 @@ def receive_message():
                 if message.get('message'):
                     #Facebook Messenger ID for user so we know where to send response back to
                     recipient_id = message['sender']['id']
+                    data['from'] = recipient_id
+                    data['to'] = message['recipient']['id']
                     if message['message'].get('text'):
+                        data['text'] = get_message()
                         response_sent_text = get_message()
                         send_message(recipient_id, response_sent_text)
                     #if user sends us a GIF, photo,video, or any other non-text item
-                    if message['message'].get('attachments'):
-                        response_sent_nontext = get_message()
-                        send_message(recipient_id, response_sent_nontext)
+                    #if message['message'].get('attachments'):
+                    #    response_sent_nontext = get_message()
+                    #    send_message(recipient_id, response_sent_nontext)
+        print(json.dumps(data))
     return "Message Processed"
 
 
